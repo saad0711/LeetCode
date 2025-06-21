dbcf0a6a7360400cc65380f4d823b6cb42e58859
saad0711 <141340151+saad0711@users.noreply.github.com>
2025-06-21 16:21:11 +0600

    Update 832. Flipping an Image.java

--- 832. Flipping an Image.java
+++ 832. Flipping an Image.java
@@ -1,3 +1,4 @@
+// 1ms time solution
 class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int len = image.length;
@@ -21,6 +22,19 @@ class Solution {
                 else{
                     image[i][j] = 1;
                 }
+            }
+        }
+        return image;
+    }
+}
+// 0ms time solution
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int arr[] : image){
+            for(int i=0;i<(image[0].length+1)/2;i++){
+                int temp = arr[i]^1;
+                arr[i] = arr[image[0].length-i-1]^1;
+                arr[image[0].length-i-1] = temp;
             }
         }
         return image;
